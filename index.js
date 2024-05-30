d82c496efac67e605d08f5bca7b2f55367170688
rehmansohail <sohailmalikraja@gmail.com>
2024-05-30 15:13:59 +0530

    added functionality to add, delete a book

--- index.js
+++ index.js
@@ -26,14 +26,17 @@ let createTaskCard = (title,author,pages,read) => {
     cardBody.appendChild(cardBodyHeading)
     cardBody.appendChild(cardBodyPages)
     let cardBodyFooter = document.createElement('div')
+    cardBodyFooter.id = "buttons"
     let btn1 = document.createElement('a')
-    btn1.href="#"
+    btn1.href="#/"
+    btn1.style.cssText="margin-right: 2px;"
     btn1.classList.add('btn', 'btn-danger')
     btn1.innerText = "Delete"
 
     let btn2 = document.createElement('a')
-    btn2.href="#"
+    btn2.href="#/"
     btn2.classList.add('btn')
+    btn2.classList.add('read')
     if(read=='yes'){
         btn2.innerText="Read"
         btn2.classList.add('btn-danger')
@@ -51,3 +54,68 @@ let createTaskCard = (title,author,pages,read) => {
     
 
 }
+
+
+
+//handling the form
+const bookForm = document.getElementById('form1')
+bookForm.addEventListener('submit',()=>{
+    event.preventDefault()
+    
+    let title = document.getElementById('inputTitle').value
+    let author = document.getElementById('inputAuthor').value
+    let pages = document.getElementById('inputPages').value
+    let read = document.getElementById('gridCheck').checked ? "yes" : "no"
+    // console.log(title)
+    // console.log(author)
+    // console.log(pages)
+    // console.log(read)
+    createTaskCard(title,author,pages,read)
+
+
+    bookForm.reset()
+})
+
+//code to remove cards 
+
+const bookContainer = document.getElementById('cardContainer')
+
+bookContainer.addEventListener('click',(e)=>{
+    
+    if(e.target.classList.contains('btn')){
+        
+        if(e.target.textContent =="Delete"){
+        const card = e.target.closest('.col');
+        if (card) {
+        bookContainer.removeChild(card);
+      }
+    }
+    if(e.target.textContent =="Read"){
+       const btns = e.target.closest("#buttons")
+       const oldbtn = e.target.closest('.read')
+       let newbtn = document.createElement('a')
+        newbtn.href="#/"
+        newbtn.classList.add('btn', 'btn-success','read')
+        newbtn.innerText = "Not Read"
+        btns.removeChild(oldbtn)
+        btns.appendChild(newbtn)
+    }
+    if(e.target.textContent=="Not Read"){
+        const btns = e.target.closest("#buttons")
+       const oldbtn = e.target.closest('.read')
+       let newbtn = document.createElement('a')
+        newbtn.href="#/"
+        newbtn.classList.add('btn', 'btn-danger','read')
+        newbtn.innerText = "Read"
+        btns.removeChild(oldbtn)
+        btns.appendChild(newbtn)
+    }
+}   
+})
+
+//prevent default anchor tag behaviour
+document.addEventListener('click',(e)=>{
+    if(e.target.tagName =='a'){
+        e.preventDefault();
+    }
+})
