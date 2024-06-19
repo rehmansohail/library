784373da6368b1ee3b627e37cb1bf3ed2fa99901
rehmansohail <sohailmalikraja@gmail.com>
2024-06-19 18:20:07 +0530

    addBook page added

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ import express from "express";
 import bodyParser from "body-parser";
 
 
+
 const app = express()
 const port = 3000
+
+
+let title =''
+let author=''
+let coverId=''
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -12,13 +18,23 @@ app.get("/",(req,res)=>{
     res.render("index.ejs")
 })
 
+
+
+
 app.get("/book", async (req,res)=>{
-  const title = req.query.title;
-  const author = req.query.author;
-  const coverId = req.query.coverId;
-  console.log(title,author,coverId)
+  title = req.query.title;
+  author = req.query.author;
+  coverId = req.query.coverId;
+  // console.log(title,author,coverId)
+  res.render("addBook.ejs",{
+    key:coverId
+  })
 })
 
+app.post("/add",(req,res)=>{
+  console.log(req.body)
+  res.send("form received")
+})
 
 app.listen(port,()=>{
     console.log(`The app is live on port ${port}`)
